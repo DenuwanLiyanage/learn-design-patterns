9d385df0c0346b8b063534e1bbef2273b48b28f2
ParagonTheDev <nipuna.sudha@gmail.com>
2022-06-15 13:28:46 +0530

    Implement adapter-pattern

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,43 @@
-class Tv{
-    public turnOn():void{
-        console.log("Tv on");
-    }
-    public turnOff():void{
-        console.log("Tv off");
-    }
-    public changeChannel():void{
-        console.log("Change tv channel");
+interface IUsbDevice {
+    receiveDataAsAndroid(data: string): void;
+}
+
+class IPhone {
+    receiveDataAsIos(data: string): void {
+        console.log(`Receive ${data} as ios Apple`)
     }
 }
 
-class DvdPlayer{
-    public turnOn():void{
-        console.log("Dvd player on");
-    }
-    public turnOff():void{
-        console.log("Dvd player off");
-    }
-    public ejectTray():void{
-        console.log("DVD player eject tray");
+class Galaxy implements IUsbDevice {
+    receiveDataAsAndroid(data: string): void {
+        console.log(`Receive ${data} as Android Galaxy`)
     }
 }
 
-class AirCondition{
-    private temp:number = 27;
-    public turnOn():void{
-        console.log("Air condition on");
+class LightningToUsbAdapter implements IUsbDevice {
+
+    public constructor(public apple: IPhone) {
     }
-    public turnOff():void{
-        console.log("Air condition off");
-    }
-    public setTemperature(temp:number):void{
-        this.temp = temp;
-        console.log(`Air condition set temperature to ${temp}`);
+
+    receiveDataAsAndroid(data: string): void {
+        this.apple.receiveDataAsIos(data.toUpperCase());
     }
 }
 
-class HomeAppliance{
-    public constructor(public tv:Tv, public dvd:DvdPlayer, public ac:AirCondition) {
+class Computer {
+    public constructor() {
     }
 
-    public setupForMovie():void{
-        console.log("Movie setup");
-        this.tv.turnOn();
-        this.dvd.turnOn();
-        this.ac.turnOn();
-        this.ac.setTemperature(25);
-    }
-    public setupForSleep():void{
-        console.log("Sleep setup");
-        this.tv.turnOff();
-        this.dvd.turnOff();
-        this.ac.setTemperature(20);
-    }
-    public setupForLeave():void{
-        console.log("Leave setup");
-        this.tv.turnOff();
-        this.dvd.turnOff();
-        this.ac.turnOff();
+    public copyToDevice(usb: IUsbDevice): void {
+        usb.receiveDataAsAndroid("saman")
     }
 }
 
-let tv = new Tv();
-let dvd = new DvdPlayer();
-let ac = new AirCondition();
-let homeAppliance = new HomeAppliance(tv, dvd, ac);
+let galaxy = new Galaxy();
+let apple = new IPhone();
+let adapter = new LightningToUsbAdapter(apple)
 
-homeAppliance.setupForMovie();
-homeAppliance.setupForSleep();
-homeAppliance.setupForLeave();
+let computer = new Computer();
+
+computer.copyToDevice(galaxy);
+computer.copyToDevice(adapter);
